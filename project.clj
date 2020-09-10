80fa1cf4eb402823f8d62654f032644f207cbee1
tom <tom@that-place>
2020-09-10 03:23:18 -0500

    Bumped to 0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject structural "0.1.2-SNAPSHOT"
+(defproject structural "0.2.0-SNAPSHOT"
   :description "A little library for defining hinted destructuring operations."
   :url "https://github.com/joinr/structural"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
